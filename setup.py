1af9ba255458e3e0d5ac1dc138516dc69dcddadb
joan <joan.herisson@univ-evry.fr>
2020-11-10 23:37:25 +0100

    build(setup): encode 'utf-8' when read files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 _readme = 'README.md'
 
-with open(_readme, 'r') as f:
+with open(_readme, 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 _release = 'RELEASE'
@@ -11,7 +11,7 @@ with open(_release, 'r') as f:
     _version = f.readline().split()[0]
 
 _extras_path = 'extras'
-with open(_extras_path+'/.env', 'r') as f:
+with open(_extras_path+'/.env', 'r', encoding='utf-8') as f:
     for line in f:
         if line.startswith('PACKAGE='):
             _package = line.splitlines()[0].split('=')[1].lower()
@@ -23,18 +23,6 @@ with open(_extras_path+'/.env', 'r') as f:
             _descr = line.splitlines()[0].split('=')[1].lower()
         if line.startswith('CORR_AUTHOR='):
             _corr_author = line.splitlines()[0].split('=')[1].lower()
-
-# from yaml import safe_load as yaml_safe_load
-# from yaml import YAMLError
-# required = []
-# with open(_extras_path+'/environment-run.yml', 'r') as run_stream:
-# # with open(_extras_path+'/environment-build.yml', 'r') as build_stream:
-#     try:
-#         required = yaml_safe_load(run_stream)['dependencies']
-#         # required += yaml_safe_load(build_stream)['dependencies']
-#     except yaml.YAMLError as exc:
-#         print(exc)
-
 
 setup(
     name                          = _package,
