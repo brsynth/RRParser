6ab4d39dd54cfcf7f9f0f206cbd3c1cb67c03c2a
joan <joan.herisson@univ-evry.fr>
2020-11-16 23:39:24 +0100

    fix(output): change output management

--- rrparser/__main__.py
+++ rrparser/__main__.py
@@ -3,19 +3,24 @@
 from logging   import error as logging_error
 from rrparser  import build_args_parser, parse_rules
 from brs_utils import compress_tar_gz
-from re        import sub
+from tempfile  import gettempdir
+from os        import path as os_path
 
 def _cli():
     parser = build_args_parser()
     args  = parser.parse_args()
 
     if args.compress: # Check if 'outfile' is required as a tar.gz archive
-        # Remove '.tar.gz' file extension
-        args.outfile = sub('\.tar.gz$', '', args.outfile)
+        outfile = os_path.join(gettempdir(), args.rules_file \
+                                        +'-'+args.rule_type  \
+                                        +'-'+args.diameters  \
+                                        +'.'+args.output_format)
+    else:
+        outfile = args.outfile
 
     try:
         results = parse_rules(rules_file=args.rules_file,
-                              outfile=args.outfile,
+                              outfile=outfile,
                               input_format=args.input_format,
                               rule_type=args.rule_type,
                               diameters=args.diameters,
@@ -24,7 +29,7 @@ def _cli():
         if results:
             print(results)
         elif args.compress: # Check if 'outfile' is required as a tar.gz archive
-            compress_tar_gz(args.outfile, delete=True)
+            compress_tar_gz(path=outfile, outFile=args.outfile, delete=True)
     except ValueError as e:
         logging_error(str(e))
 
